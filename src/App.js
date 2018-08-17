9aaaee00238dff6cb865c4bfa8188ea7295e4e37
Danshendaisy <37489386+Danshendaisy@users.noreply.github.com>
2018-08-16 18:16:52 -0700

    First commit

---
+++ src/App.js
@@ -0,0 +1,40 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+import axios from 'axios'
+
+class App extends Component {
+  state ={
+    orders:[]
+  }
+
+  componentDidMount(){
+    axios.get(`/driverOrders`,
+    { crossdomain: true })
+    .then(res => {
+      console.log(res)
+      const orders= res.data.order
+      this.setState({orders})
+      console.log(orders)
+    })
+  }
+
+  render() {
+    return (
+      
+        <div style={{marginLeft: 10, alignItems:'flex-start', justifyContent:'center'}}>
+          {this.state.orders.map(order =>
+            <p style={{fontWeight:'bold'}}>{order.userName} phone: {order.phone}
+            <ul>
+              {order.items.map(item=>
+              <li>{item.name} {item.count}</li>)}
+          </ul>
+          </p>
+          )}
+        </div>
+      
+    );
+  }
+}
+
+export default App;
